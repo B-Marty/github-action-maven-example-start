4576dff6976498e8be192a721f90a7169a05ce88
Marty <53341619+B-Marty@users.noreply.github.com>
2022-04-13 10:43:51 +0200

    Update TestDeepThought.java

--- src/test/java/TestDeepThought.java
+++ src/test/java/TestDeepThought.java
@@ -17,5 +17,4 @@ public class TestDeepThought {
         DeepThought superComputer = new DeepThought();
         assertEquals( 42 , superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
     }
-
 }
